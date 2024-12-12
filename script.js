cd42a729112841ec09338910722da19415249acf
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2024-12-12 09:13:11 -0300

    -Adição da Biblioteca UIKit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-  $("#intro").ready(()=>{
-    $("#intro").addClass("visible");
-  })
+
+   $("#lego").on("click",function(){
+      $("html body *").addClass("border border-warning rounded ui-widget-content d-inline-block user-select-none")
+      $("html body *").draggable();
+   })
 });
