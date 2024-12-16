2be2465dace922f9897b40eb3fab28d00e584dc6
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2024-12-16 16:49:47 -0300

    -Adição da Biblioteca UIKit

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
       $("html body *").addClass("border border-warning rounded ui-widget-content d-inline-block user-select-none")
       $("html body *").draggable();
    })
+
+   $("#item1").click(function() {
+      $('#itens').animate({
+        scrollTop: $("#tst1").position().top
+      }, 500);
+    });
+    $("#item2").click(function() {
+      $('#itens').animate({
+        scrollTop: $("#tst2").position().top
+      }, 500);
+    });
+    $("#item3").click(function() {
+      $('#itens').animate({
+        scrollTop: $("#tst3").position().top
+      }, 500);
+    });
 });
