321702ad49e755c323d0cb3fd0fbc77ee1dda2a6
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2024-12-18 16:41:41 -0300

    -Tentativa efeito slide

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
 
 
-   $("#lego").on("click",function(){
-      $("html body *").addClass("border border-warning rounded ui-widget-content d-inline-block user-select-none")
-      $("html body *").draggable();
-   })
+  $("#item1").click(function() {
+    $("#itens").animate(
+      {
+          scrollTop: $("#tst1").offset().top - $("#itens").offset().top,
+      },
+      1000
+  );
+});
 
-   $("#item1").click(function() {
-      $('#itens').animate({
-        scrollTop: $("#tst1").position().top
-      }, 500);
-    });
-    $("#item2").click(function() {
-      $('#itens').animate({
-        scrollTop: $("#tst2").position().top
-      }, 500);
-    });
-    $("#item3").click(function() {
-      $('#itens').animate({
-        scrollTop: $("#tst3").position().top
-      }, 500);
-    });
+$("#item2").click(function() {
+  $("#itens").animate(
+    {
+        scrollTop: $("#tst2").offset().top - $("#itens").offset().top,
+    },
+    1000
+);
+});
+
+$("#item3").click(function() {
+  $("#itens").animate(
+    {
+        scrollTop: $("#tst3").offset().top - $("#itens").offset().top,
+    },
+    1000
+);
+});
 });
