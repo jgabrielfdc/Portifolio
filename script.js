5c7f14f1ff4ceb81c27c56f6ced7730d17494625
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2024-12-19 10:23:11 -0300

    -Refatoração

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
 
 
-  $("#item1").click(function() {
-    $("#itens").animate(
-      {
-          scrollTop: $("#tst1").offset().top - $("#itens").offset().top,
-      },
-      1000
-  );
-});
-
-$("#item2").click(function() {
-  $("#itens").animate(
-    {
-        scrollTop: $("#tst2").offset().top - $("#itens").offset().top,
-    },
-    1000
-);
-});
-
-$("#item3").click(function() {
-  $("#itens").animate(
-    {
-        scrollTop: $("#tst3").offset().top - $("#itens").offset().top,
-    },
-    1000
-);
-});
 });
