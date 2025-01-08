41f5f4cbe0bce66c5ace3d27bcd3101660027552
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2025-01-08 11:29:41 -0300

    -Exclusão de Função Redundante

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -1,18 +1,11 @@
-function skill(archive){
-    $('#resumo_skills').load("assets/components/"+archive+"_content.html");
-    $('#resumo_skills').addClass("pb-3")
-}
-
 $(document).ready(function(){
 
     $("#habilidades .uk-card").each(function(){
         $(this).on("click",function(){
             let archive=$(this).attr("id");
-            skill(archive);
+            $('#resumo_skills').load("assets/components/"+archive+"_content.html");
+            $('#resumo_skills').addClass("pb-3")
         })
-
-            console.log(this);
-            console.log($(this).attr("id"));
     })
 
 })
