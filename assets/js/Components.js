b4422dbfaf6d448b21d8f4b19cd7e404519baa37
JGabrielFDC <jgabrielfdc08@gmail.com>
2025-09-22 21:02:56 -0300

    feat: Introdução

---
+++ assets/js/Components.js
@@ -0,0 +1,4 @@
+$(document).ready(()=>{
+    $("#intro__container").load('assets/components/sections/Intro.html')
+    $("#biografia__container").load('assets/components/sections/Biografia.html')
+})
